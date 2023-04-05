f7754135e45ed026ab92f8c2ad4499ed1e4cd037
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-04-06 01:20:30 +0600

    increase decrease done

--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@ function updateCaseNumber(isIncrease) {
   const caseNumberField = document.getElementById("case-number-field");
   const caseNumberString = caseNumberField.value;
   const previousCaseNumber = parseInt(caseNumberString);
-  const newCaseNumber = previousCaseNumber + 1;
+
+  let newCaseNumber;
+  if(isIncrease === true){
+    newCaseNumber = previousCaseNumber + 1;
+  }
+  else{
+    newCaseNumber = previousCaseNumber - 1;
+  }
+
   caseNumberField.value = newCaseNumber;
 
   /* let newCaseNumber;
@@ -24,10 +32,10 @@ function updateCaseNumber(isIncrease) {
 }
 
 document.getElementById("btn-case-plus").addEventListener("click", function () {
-  updateCaseNumber();
+  updateCaseNumber(true);
 
 });
 document.getElementById("btn-case-minus").addEventListener("click", function () {
-    updateCaseNumber();
+    updateCaseNumber(false);
 
 });
