2fef8f7b78141df9c0ec532308fcbb43926bf964
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-04-07 03:49:59 +0600

    calculate sub total

--- js/phone.js
+++ js/phone.js
@@ -1,6 +1,8 @@
 document.getElementById('btn-phone-plus').addEventListener('click', function(){
     const newPhoneNumber = updateNumber(true, 'phone-number-field');
     updateTotalPrice(newPhoneNumber, 'phone-total', 1219);
+
+    // calculate subtotal
 });
 
 document.getElementById('btn-phone-minus').addEventListener('click', function(){
